64f1d3d9b33e6fa13e899e615c9160bb74ff5e8d
Charlie Mathes <comathes18@gmail.com>
2025-05-12 12:38:10 -0400

    Added cleaned_telco_churn; Added day 2 sql queries;  Checkpoint: data explored in SQL and then cleaned in python, created cleaned version for analysis, and loaded into PostgreSQL

--- sql/day_2_exploratory_queries.sql
+++ sql/day_2_exploratory_queries.sql
@@ -13,3 +13,34 @@ SELECT "Churn",
        ROUND(MAX("MonthlyCharges")::numeric, 2) AS max_monthly
 FROM telco_churn
 GROUP BY "Churn";
+
+
+-- Query 3: Churn Rate by Internet Service Type [fiber optics]
+SELECT "InternetService", COUNT(*) AS total_customers,
+       SUM(CASE WHEN "Churn" = 'Yes' THEN 1 ELSE 0 END) AS churned,
+       ROUND(100.0 * SUM(CASE WHEN "Churn" = 'Yes' THEN 1 ELSE 0 END) / COUNT(*), 2) AS churn_rate
+FROM telco_churn
+GROUP BY "InternetService"
+ORDER BY churn_rate DESC;
+
+-- Query 4: Churn Rate by Payment Method [electronic check]
+SELECT "PaymentMethod", COUNT(*) AS total_customers,
+       SUM(CASE WHEN "Churn" = 'Yes' THEN 1 ELSE 0 END) AS churned,
+       ROUND(100.0 * SUM(CASE WHEN "Churn" = 'Yes' THEN 1 ELSE 0 END) / COUNT(*), 2) AS churn_rate
+FROM telco_churn
+GROUP BY "PaymentMethod"
+ORDER BY churn_rate DESC;
+
+-- Query 5: Determine Distinct Values for Categorical Columns to Standardize Table
+SELECT DISTINCT "Partner" FROM telco_churn;
+SELECT DISTINCT "Dependents" FROM telco_churn;
+SELECT DISTINCT "PhoneService" FROM telco_churn;
+SELECT DISTINCT "PaperlessBilling" FROM telco_churn;
+SELECT DISTINCT "Churn" FROM telco_churn;
+SELECT DISTINCT "MultipleLines" FROM telco_churn;
+SELECT DISTINCT "OnlineSecurity" FROM telco_churn;
+SELECT DISTINCT "OnlineBackup" FROM telco_churn;
+SELECT DISTINCT "DeviceProtection" FROM telco_churn;
+SELECT DISTINCT "TechSupport" FROM telco_churn;
+SELECT DISTINCT "StreamingTV" FROM telco_churn;
+SELECT DISTINCT "StreamingMovies" FROM telco_churn;
